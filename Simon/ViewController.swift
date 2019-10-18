f456905fd48984dcb1d7660834e9c475dc5d3246
Eduardo Mendoza <rl238513@stu.rlas-116.org>
2019-10-18 12:11:53 -0500

    create the brain of the app

--- Simon/ViewController.swift
+++ Simon/ViewController.swift
@@ -7,16 +7,26 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var background: UIView!
+    @IBOutlet weak var colorsFrame: UIView!
     @IBOutlet weak var simonLabel: UILabel!
-    @IBOutlet weak var score: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet var colorDisplays: [UIView]!
+    @IBOutlet weak var startButton: UIButton!
+    
+    var sound: AVAudioPlayer?
+    var timer = Timer()
+    var pattern = [Int]()
+    var index = 0
+    var playerTurn = false
+    var gameOver = true
+    
     
     override func viewDidLoad() {
-        self.background.backgroundColor = .black
+        self.colorsFrame.backgroundColor = .black
         // Do any additional setup after loading the view.
     }
 
@@ -25,5 +35,56 @@ class ViewController: UIViewController {
     }
     
     @IBAction func onColorTapped(_ sender: UITapGestureRecognizer) {
+        for number in 0..<colorDisplays.count{
+            if colorDisplays[number].frame.contains(sender.location(in: colorsFrame)) {
+                flashColor(number: number)
+                index += 1
+            }
+        }
+    }
+    func playSound(fileName: String) {
+        if let path = Bundle.main.path(forResource: fileName, ofType: "wav") {
+            let url = URL(fileURLWithPath: path)
+            do{
+                self.sound = try AVAudioPlayer(contentsOf: url)
+                self.sound?.play()
+            }
+            catch {
+                print("Can't find file")
+            }
+        }
+    }
+    func addToPattern() {
+        pattern.append(Int(arc4random_uniform(4)))
+    }
+    func restart() {
+        pattern.removeAll()
+        index = 0
+        addToPattern()
+        startButton.alpha = 1.0
+    }
+    func flashColor(number: Int) {
+        self.playSound(fileName: String(number))
+        UIView.transition(with: colorDisplays[number], duration: 0.2, options: .transitionCrossDissolve, animations: {
+            self.colorDisplays[number].alpha = 1.0
+        }) { (true) in
+            UIView.transition(with: self.colorDisplays[number], duration: 0.2, options: .transitionCrossDissolve, animations: { self.colorDisplays[number].alpha = 0.4
+            }, completion: nil)
+        }
+    }
+    func displayPattern() {
+        timer = Timer()
+    }
+    @objc func nextColor() {
+        if index < pattern.count {
+            flashColor(number: pattern[index])
+            index += 1
+        }
+        else {
+            timer.invalidate()
+            index = 0
+            playerTurn = true
+            messageLable.text = "Your Turn"
+        }
     }
 }
